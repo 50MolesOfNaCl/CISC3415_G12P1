2ac805a8b49c672955b028090bb30933176630c6
50MolesOfNaCl <edmund.lam2@gmail.com>
2020-09-12 21:41:27 -0400

    untested change to turnrate!

--- project1/roomba-roam.cc
+++ project1/roomba-roam.cc
@@ -1,9 +1,9 @@
 /*
  *  roomba-roam.cc
- * 
+ *
  *  Sample code for a robot that is suitable for use with the Roomba
  *  and Create.
- * 
+ *
  *  Based on an example provided by Monica Anderson and Jeff Forbes,
  *  via Carlos Jaramillo, and changed to (hopefully) make it easier to
  *  understand.
@@ -11,7 +11,7 @@
  *  Modified:    Simon Parsons
  *  Date:        15th June 2009
  *  Last change: 20th September 2011
- *  
+ *
  */
 #define _USE_MATH_DEFINES
 
@@ -19,16 +19,17 @@
 #include <cstdlib>
 #include <libplayerc++/playerc++.h>
 #include <math.h>
+#include <iomanip> //for setprecision( i )
 
 int main(int argc, char *argv[])
-{  
-  using namespace PlayerCc;  
+{
+  using namespace PlayerCc;
 
   // Set up proxy. Proxies are the datastructures that Player uses to
   // talk to the simulator and the real robot.
 
-  PlayerClient    robot("localhost");  
-  Position2dProxy pp(&robot,0);       // The 2D proxy is used to send 
+  PlayerClient    robot("localhost");
+  Position2dProxy pp(&robot,0);       // The 2D proxy is used to send
                                       // motion commands.
 
   int timer = 0;                      // A crude way to time what we do
@@ -43,10 +44,10 @@ int main(int argc, char *argv[])
   int forwardInterval = 30 / defaultSpeed;  //0 to forwardInterval, adjust for defaultSpeed so that it reaches 3 meters i.e. move forward for 3 seconds if speed is 1, and for 6 seconds if defaultSpeed is .5
 
 
-  double turnInterval = 90; 	      //interval1 to interval2, i.e. 9 seconds. 
+  double turnInterval = 90; 	      //interval1 to interval2, i.e. 9 seconds.
 
   //How many complete square robot should make
-  double numSquares = 1; 	              // numTurns * 4 = 1 square 
+  double numSquares = 1; 	              // numTurns * 4 = 1 square
 			 	      // numOfSquares * x, for when you want multiple runs.
 
 
@@ -54,8 +55,8 @@ int main(int argc, char *argv[])
   pp.SetMotorEnable(true);
 
   // Control loop
-  while(true) 
-    {    
+  while(true)
+    {
 
       // Increment the counter.
       timer++;
@@ -63,20 +64,20 @@ int main(int argc, char *argv[])
       // Read from the proxies.
       robot.Read();
 
-      // First make the rbot go straight ahead, then make it turn, and 
+      // First make the rbot go straight ahead, then make it turn, and
       // finally make it stop.
 
 
  		if(timer < forwardInterval ){
 		//0 to forwardInterval, Go forward
-		speed = defaultSpeed; 
-		turnrate = 0; 
+		speed = defaultSpeed;
+		turnrate = 0;
      		}
     		else
       		if((timer >= forwardInterval ) && (timer < turnInterval)){
 		//forwardInterval to turnInterval, Start turning
 		speed = 0;
-		turnrate = -(M_PI/2)/(turnInterval - forwardInterval); //ties turnrate to time, i.e. 90 degrees distributed over this interval. The greater the diff the slower it turns
+		turnrate = -(M_PI/2)/((turnInterval - forwardInterval)/10); //ties turnrate to time, i.e. 90 degrees distributed over this interval. The greater the diff the slower it turns
        		 }
        		else{ //timer == forwardInterval
 		timer = 0; //reset timer
@@ -86,19 +87,17 @@ int main(int argc, char *argv[])
 		if(numTurns == 4*numSquares) break;
         std::cout << "----------------------------------"<< std::endl;
         	}
-	
-     
-      
+
+
+
       // Print out what we decided to do?
       std::cout << "Turn: " << (numTurns/4)+1 << speed << std::endl;
-      std::cout << " | Timer: " << timer << std::endl;  
-      std::cout << " | Speed: " << speed << std::endl;      
-      std::cout << " | Turn rate: " << turnrate << std::endl << std::endl;
-    
+      std::cout << " | Timer: " << timer << std::endl;
+      std::cout << " | Speed: " << speed << std::endl;
+      std::cout << " | Turn rate: " << setprecision( 10 ) << turnrate << std::endl << std::endl;
+
       // Send the motion commands that we decided on to the robot.
-      pp.SetSpeed(speed, turnrate);  
+      pp.SetSpeed(speed, turnrate);
     }
-  
+
 }
-
-
